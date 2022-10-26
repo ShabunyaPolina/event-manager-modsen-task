313c1f8f2ac877cd52354b149590dcadc8054720
ShabunyaPolina <shabunya.polina@main.ru>
2022-10-26 21:59:34 +0300

    add event controller

---
+++ src/main/java/by/shabunya/eventmanager/controller/EventController.java
@@ -0,0 +1,45 @@
+package by.shabunya.eventmanager.controller;
+
+import by.shabunya.eventmanager.entity.Event;
+import by.shabunya.eventmanager.service.EventService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import java.util.List;
+
+@RestController
+public class EventController {
+
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping("/events")
+    public List<Event> getEventList() {
+        return eventService.getEventList();
+    }
+
+    @GetMapping("/events/{id}")
+    public Event getEventById(@PathVariable("id") Long eventId) {
+        return eventService.getEventById(eventId);
+    }
+
+    @PostMapping("/events")
+    public Event saveEvent(@Valid @RequestBody Event event) {
+        return eventService.saveEvent(event);
+    }
+
+    @PutMapping("/events/{id}")
+    public Event updateEvent(@RequestBody Event event,
+                             @PathVariable("id") Long eventId) {
+        return eventService.updateEvent(event, eventId);
+    }
+
+    @DeleteMapping("/events/{id}")
+    public void deleteEvent(@PathVariable("id") Long eventId) {
+        eventService.deleteEvent(eventId);
+    }
+}
